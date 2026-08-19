3b2a77a149560ff08e388336dfaabe10e9bee4ea
Dinesh Nandi <dineshnandi07@gmail.com>
2026-08-19 17:43:20 +0530

    completed cloning myntra

---
+++ 3.JS/Project/data.js
@@ -0,0 +1,129 @@
+const items = [
+  {
+    id: 1,
+    image: "../Images/myntra/1.jpg",
+    alt: "Img1",
+    rating: {
+      stars: 4.5,
+      noOfStars: 1400,
+    },
+    heading: "Carlton London",
+    para: "Rhodium-Plated CZ Floral Studs",
+    current_price: "Rs 606",
+    original_price: "Rs 1045",
+    discount_price: "(42% OFF)",
+    delivery: "10 Oct 2023",
+  },
+
+  {
+    id: 2,
+    image: "../Images/myntra/2.jpg",
+    alt: "Img2",
+    rating: {
+      stars: 4.3,
+      noOfStars: 24,
+    },
+    heading: "CUKOO",
+    para: "Women Padded Halter Neck Swimmin...",
+    current_price: "Rs 1507",
+    original_price: "Rs 2599",
+    discount_price: "(42% OFF)",
+    delivery: "12 Oct 2023",
+  },
+
+  {
+    id: 3,
+    image: "../Images/myntra/3.jpg",
+    alt: "Img3",
+    rating: {
+      stars: 4.1,
+      noOfStars: 249,
+    },
+    heading: "NUEVOSDAMAS",
+    para: "Women Red & White Printed A-Line Kne...",
+    current_price: "Rs 495",
+    original_price: "Rs 1599",
+    discount_price: "(69% OFF)",
+    delivery: "14 Oct 2023",
+  },
+
+  {
+    id: 4,
+    image: "../Images/myntra/4.jpg",
+    alt: "Img4",
+    rating: {
+      stars: 5,
+      noOfStars: 10,
+    },
+    heading: "ADIDAS",
+    para: "Indian Cricket ODI Jersey",
+    current_price: "Rs 999",
+    original_price: "Rs 999",
+    discount_price: "(0% OFF)",
+    delivery: "11 Oct 2023",
+  },
+
+  {
+    id: 5,
+    image: "../Images/myntra/5.jpg",
+    alt: "Img5",
+    rating: {
+      stars: 4.2,
+      noOfStars: 3500,
+    },
+    heading: "Roadster",
+    para: "Pure Cotton T-shirt",
+    current_price: "Rs 489",
+    original_price: "Rs 1399",
+    discount_price: "(65% OFF)",
+    delivery: "13 Oct 2023",
+  },
+
+  {
+    id: 6,
+    image: "../Images/myntra/6.jpg",
+    alt: "Img6",
+    rating: {
+      stars: 0,
+      noOfStars: 0,
+    },
+    heading: "Nike",
+    para: "Men ReactX Running Shoes",
+    current_price: "Rs 14995",
+    original_price: "Rs 14995",
+    discount_price: "(0% OFF)",
+    delivery: "15 Oct 2023",
+  },
+
+  {
+    id: 7,
+    image: "../Images/myntra/7.jpg",
+    alt: "Img7",
+    rating: {
+      stars: 4.2,
+      noOfStars: 388,
+    },
+    heading: "The Indian Garage Co",
+    para: "Men Slim Fit Regular Shorts",
+    current_price: "Rs 639",
+    original_price: "Rs 1599",
+    discount_price: "(60% OFF)",
+    delivery: "10 Oct 2023",
+  },
+
+  {
+    id: 8,
+    image: "../Images/myntra/8.jpg",
+    alt: "Img8",
+    rating: {
+      stars: 4.2,
+      noOfStars: 5200,
+    },
+    heading: "Nivea",
+    para: "Men Fresh Deodorant 150ml",
+    current_price: "Rs 142",
+    original_price: "Rs 205",
+    discount_price: "(50% OFF)",
+    delivery: "16 Oct 2023",
+  },
+];
--- 3.JS/Project/myntra.js
+++ 3.JS/Project/myntra.js
@@ -1,125 +1,5 @@
 let itemsElement = document.querySelector('.products');
-const items = [
-  {
-    id: 1,
-    image: "../Images/myntra/1.jpg",
-    alt: "Img1",
-    rating: {
-      stars: 4.5,
-      noOfStars: 1400,
-    },
-    heading: "Carlton London",
-    para: "Rhodium-Plated CZ Floral Studs",
-    current_price: "Rs 606",
-    original_price: "Rs 1045",
-    discount_price: "(42% OFF)",
-  },
 
-  {
-    id: 2,
-    image: "../Images/myntra/2.jpg",
-    alt: "Img2",
-    rating: {
-      stars: 4.3,
-      noOfStars: 24,
-    },
-    heading: "CUKOO",
-    para: "Women Padded Halter Neck Swimmin...",
-    current_price: "Rs 1507",
-    original_price: "Rs 2599",
-    discount_price: "(42% OFF)",
-  },
-
-  {
-    id: 3,
-    image: "../Images/myntra/3.jpg",
-    alt: "Img3",
-    rating: {
-      stars: 4.1,
-      noOfStars: 249,
-    },
-    heading: "NUEVOSDAMAS",
-    para: "Women Red & White Printed A-Line Kne...",
-    current_price: "Rs 495",
-    original_price: "Rs 1599",
-    discount_price: "(69% OFF)",
-  },
-
-  {
-    id: 4,
-    image: "../Images/myntra/4.jpg",
-    alt: "Img4",
-    rating: {
-      stars: 5,
-      noOfStars: 10,
-    },
-    heading: "ADIDAS",
-    para: "Indian Cricket ODI Jersey",
-    current_price: "Rs 999",
-    original_price: "Rs 999",
-    discount_price: "(0% OFF)",
-  },
-
-  {
-    id: 5,
-    image: "../Images/myntra/5.jpg",
-    alt: "Img5",
-    rating: {
-      stars: 4.2,
-      noOfStars: 3500,
-    },
-    heading: "Roadster",
-    para: "Pure Cotton T-shirt",
-    current_price: "Rs 489",
-    original_price: "Rs 1399",
-    discount_price: "(65% OFF)",
-  },
-
-  {
-    id: 6,
-    image: "../Images/myntra/6.jpg",
-    alt: "Img6",
-    rating: {
-      stars: 0,
-      noOfStars: 0,
-    },
-    heading: "Nike",
-    para: "Men ReactX Running Shoes",
-    current_price: "Rs 14995",
-    original_price: "Rs 14995",
-    discount_price: "(0% OFF)",
-  },
-
-  {
-    id: 7,
-    image: "../Images/myntra/7.jpg",
-    alt: "Img7",
-    rating: {
-      stars: 4.2,
-      noOfStars: 388,
-    },
-    heading: "The Indian Garage Co",
-    para: "Men Slim Fit Regular Shorts",
-    current_price: "Rs 639",
-    original_price: "Rs 1599",
-    discount_price: "(60% OFF)",
-  },
-
-  {
-    id: 8,
-    image: "../Images/myntra/8.jpg",
-    alt: "Img8",
-    rating: {
-      stars: 4.2,
-      noOfStars: 5200,
-    },
-    heading: "Nivea",
-    para: "Men Fresh Deodorant 150ml",
-    current_price: "Rs 142",
-    original_price: "Rs 205",
-    discount_price: "(50% OFF)",
-  },
-];
 
 if(itemsElement){
   let productsHTML = "";
@@ -157,34 +37,46 @@ if(itemsElement){
 
 
 let addToBagElement = document.querySelectorAll('.bag-btn');
-let bagCount = document.querySelector(".carry-bag").querySelector("sup"); 
-let bagItem = []
 
-let count = Number(localStorage.getItem("Items")) || 0;
+
+let bagItems = JSON.parse(localStorage.getItem("BagItems")) || [];
+let bagCount = document.querySelector(".carry-bag").querySelector("sup"); 
+
+let count = bagItems.length;
 
 if(count > 0){
   bagCount.innerHTML = count;
   bagCount.classList.add('js-itemsInBag');
 }
-// let itemsInBag = event => {
-//   count++;
-//   bagCount.classList.add('js-itemsInBag');
-//   bagCount.innerHTML = count;
-//   localStorage.setItem("Items" , `${count}`);
-// };
 
-let itemsInBag = (event) => {
-    console.log(event.target.dataset.id);
+let itemsInBag = event => {
+
+  let id = Number(event.target.dataset.id);
+
+  bagItems.push(id);
+
+  localStorage.setItem("BagItems", JSON.stringify(bagItems));
+
+  bagCount.classList.add("js-itemsInBag");
+  bagCount.innerHTML = bagItems.length;
 };
-
 
 addToBagElement.forEach(val => {
   val.addEventListener('click' , itemsInBag);
 });
 
-let bagItems = JSON.parse(localStorage.getItem("BagItems")) || [];
+function updateBagCount(){
+  let bagItems = JSON.parse(localStorage.getItem("BagItems")) || [];
 
-bagItems.forEach(index => {
-    console.log(items[index]);
-});
+  if (bagItems.length > 0) {
+    bagCount.innerHTML = bagItems.length;
+    bagCount.classList.add("js-itemsInBag");
+    } else {
+      bagCount.innerHTML = "";
+      bagCount.classList.remove("js-itemsInBag");
+    }
+}
 
+updateBagCount();
+window.addEventListener("focus", updateBagCount);
+window.addEventListener("pageshow", updateBagCount);
